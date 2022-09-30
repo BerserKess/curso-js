1f427939b596fb9b5c8a2dfbe94cba67cb6a76b1
BerserKess <kesleyrainery.21@gmail.com>
2022-09-30 01:28:00 -0300

    atualizando

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+let qualquer = "Teste";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 89.554;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nomes de variaveis genericos e siglas
+let valor = 1;
+let numero = 1;
+let pqp = ''; // Produto Quimico Perigoso
+
+// Prefira código claros a comentários de código
