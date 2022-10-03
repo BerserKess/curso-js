abc858c96a211d7cd54f8e9d8c19daa0604a9fa0
BerserKess <kesleyrainery.21@gmail.com>
2022-10-02 23:03:05 -0300

    tipos de dados

---
+++ fundamentos/tipos-de-dados/tipoObject.js
@@ -0,0 +1,16 @@
+/**
+ * Um Objeto em JS é um grupo de pares chave e valor
+ * Objeto não é igual a  um arquivo Json
+ */
+const prod1 = {};
+prod1.nome = 'Celular Ultra Mega';
+prod1.preco = 4998.90;
+prod1['desconto'] = 0.40;
+console.log(prod1);
+
+const prod2 ={
+    nome: 'Camisa',
+    preco: 79.90
+}
+
+console.log(prod2)
